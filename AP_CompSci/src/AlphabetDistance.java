fa2e05fbe22ea6b94f5274d7a9744158b55ad548
Daniel Wart <dman.wart@gmail.com>
2015-08-26 11:48:22 -0700

    test

---
+++ AP_CompSci/src/AlphabetDistance.java
@@ -0,0 +1,17 @@
+
+public class AlphabetDistance {
+
+	public static void main(String[] args) {
+
+		String input = "test";
+		char[] inputArray = input.toCharArray();
+		
+		for(int i = 0; i<inputArray.length; i++){
+			
+			System.out.println(Character.getNumericValue(inputArray[i]));
+			
+		}
+		
+	}
+
+}
