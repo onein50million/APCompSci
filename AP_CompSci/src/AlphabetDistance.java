ae4fe6fb64613b0658844d67e3e3eaa4ad9d7c74
Daniel Wart <dman.wart@gmail.com>
2015-08-28 12:07:25 -0700

    Alphabet Distances done, including a WindowBuilder Test

--- AP_CompSci/src/AlphabetDistance.java
+++ AP_CompSci/src/AlphabetDistance.java
@@ -1,17 +1,34 @@
-
 public class AlphabetDistance {
 
 	public static void main(String[] args) {
 
-		String input = "test";
-		char[] inputArray = input.toCharArray();
 		
-		for(int i = 0; i<inputArray.length; i++){
-			
-			System.out.println(Character.getNumericValue(inputArray[i]));
-			
-		}
 		
 	}
+	
+	
+	public static void distance(String input) {
+		input = input.toUpperCase();
+		String inputSplit[] = input.split(" ");
+
+		if (inputSplit.length > 2) {
+			System.out.println("more than two strings");
+		}
+
+		char[] charArray0 = inputSplit[0].toCharArray();
+		char[] charArray1 = inputSplit[1].toCharArray();
+
+		for (int i = 0; i < charArray0.length; i++) {
+
+			if (charArray1[i] >= charArray0[i]) {
+				System.out.print(charArray1[i] - charArray0[i] + " ");
+			} else {
+				System.out.print(charArray1[i] + 26 - charArray0[i] + " ");
+			}
+
+		}
+	}
+	
+	
 
 }
