dd519ede90ff99c25d021d3303324fb02d20b158
onein50million <dman.wart@gmail.com>
2015-11-15 19:42:15 -0800

    finished pollard's rho

--- AP_CompSci/src/chapter6/Chapter6_3.java
+++ AP_CompSci/src/chapter6/Chapter6_3.java
@@ -10,20 +10,20 @@ public class Chapter6_3 {
 	
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter an integer: ");
-		int input = scan.nextInt();
+		long input = scan.nextLong();
 		scan.close();
 		System.out.println("factors:");
 		factor(input,2,2);
 	}
 
-	public static int f(int x){
+	public static long f(long x){
 		
 		return x*x+1;
 		
 	}
 	
-	public static boolean primeCheck(int number){
-		for(int i = 2; i<=Math.sqrt(number);i++){
+	public static boolean primeCheck(long number){
+		for(long i = 2; i<=Math.sqrt(number);i++){
 			if(number % i == 0){
 				return false;
 			}
@@ -31,45 +31,48 @@ public class Chapter6_3 {
 		return true;
 	}
 	
-	public static void factor(int input,int a,int b){
+	public static void factor(long input,long a,long b){
 				
 		if(primeCheck(input)){
 			System.out.println("Prime number");
 			return;
 		}
 
-		int x = a;
-		int y = b;
+
+		long x = a;
+		long y = b;
 		
-		int factor = 1;
-		while (factor == 1) {
-			x = f(x) % input;
-			y = f(f(y))% input;
-//			System.out.println("x:" + x + " y:" + y);				
-//			System.out.println(gcd(Math.abs(y-x),input));
-			factor = gcd(Math.abs(y-x),input);		
-			
+
+		
+		long factor = 1;
+		if(input%2 == 0){
+			factor = 2;
+		}
+		else{
+			while (factor == 1) {
+				x = f(x) % input;
+				y = f(f(y))% input;
+	//			System.out.println("x:" + x + " y:" + y);				
+	//			System.out.println(gcd(Math.abs(y-x),input));
+				factor = gcd(Math.abs(y-x),input);			
 			}
-//		if(primeCheck(factor)){
-//			System.out.println(factor);
-//		}
-//		else{
-//			x++;
-//			y++;
-//			factor(factor,a+1,b+1);
-//		}
-//		if(primeCheck(input/factor)){
-//			System.out.println(input/factor);
-//		}
-//		else{
-//			x++;
-//			y++;
-//			factor(input/factor,a+1,b+1);
-//		}
-		System.out.println(factor);
+		}
+		if(primeCheck(factor)){
+			System.out.println(factor);
+		}
+		else{
+			factor(factor,a+1,b+1);
+		}
+		if(primeCheck(input/factor)){
+			System.out.println(input/factor);
+		}
+		else{
+			factor(input/factor,a+1,b+1);
+		}
+//		System.out.println(factor);
 	}
 	
-	public static int gcd(int value1, int value2) {
+	public static long gcd(long value1, long value2) {
 
 		while (value1 > 0 && value2 > 0) {
 			if (value1 >= value2) {
