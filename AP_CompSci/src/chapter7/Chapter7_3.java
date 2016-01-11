ccbab19b1a3a253db6dd18f77d0f3c50c9dcabd0
onein50million <dman.wart@gmail.com>
2016-01-10 18:34:52 -0800

    added longer way to ch 7_3

--- AP_CompSci/src/chapter7/Chapter7_3.java
+++ AP_CompSci/src/chapter7/Chapter7_3.java
@@ -8,6 +8,10 @@ public class Chapter7_3 {
 		double[] array = {8, 4, 5, 21, 7, 9, 18, 2, 100};
 		array = rotN(array,3);
 		System.out.println(Arrays.toString(array));
+		//longer way below
+		for(double value:array){
+			System.out.print(value + " ");
+		}
 		
 	}
 	private static double[] rotN(double[] array, int rotNum){
