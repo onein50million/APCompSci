23f6f1118b0879dabbd0973792cbcaaaf52db248
Daniel Wart <dman.wart@gmail.com>
2016-05-17 12:08:43 -0700

    more field

--- AP_CompSci/coop/mowing/Field.java
+++ AP_CompSci/coop/mowing/Field.java
@@ -8,6 +8,8 @@ public class Field {
 	private int[][] field;
 	static final int GRASS = 0;
 	static final int TREE = 1;
+	static final int ACCESS = 2;
+	static final int STOP = 3;
 
 	// True for tree, false if grass
 
@@ -20,29 +22,41 @@ public class Field {
 	}
 
 	public String toString() {
-		String output = "   ";
+		String output = "  ";
 		for(int i = 0; i < field[0].length; i++){
-			output += i + ")";
+			output += String.format("%2d", i);
 		}
 		output += "\n";
 		for (int row = 0; row < field.length; row++) {
 
-			output += row + ") ";
+			output += String.format("%2d", row);
 			for (int column = 0; column < field[0].length; column++) {
-				output += field[row][column] + " ";
+				output += toEmoji(field[row][column]) + " ";
 			}
 			output += "\n";
 		}
 		return output;
 	}
+	private String toEmoji(int i){
+		if(i == GRASS)
+			return "♒";
+		if(i == TREE)	
+			return "❤";
+		if(i == STOP)
+			return "☎";
+		if(i == ACCESS)
+			return "✳";
+		else
+			return " ";
+	}
 
 	public List<int[]> getNear( int r, int c){
 		List<int[]> nearby = new ArrayList<int[]>(); 
-		for(int row = -2; row <= 2; row++){
-			for(int column = -2; column<=2; column++){
-				if(column == 0 && row == 0)
-					continue;
-				if((row+r < 0 || row+r >= field.length) || (column+c < 0 || column+c >= field[0].length))
+		for(int row = -3; row <= 3; row++){
+			for(int column = -3; column<= 3; column++){
+//				if(column == 0 && row == 0)
+//					continue;
+				if(row+r < 0 || row+r >= field.length || column+c < 0 || column+c >= field[0].length)
 					continue;
 				if(get(row+r,column+c)==TREE){
 					nearby.add(new int[]{row+r,column+c});
@@ -52,7 +66,7 @@ public class Field {
 		return nearby;
 	}
 	public void drawConnection(int row1, int column1, int row2, int column2){
-		int fill = 2;
+		int fill = STOP;
 		for(int column = Math.min(column1, column2); column <= Math.max(column1, column2); column += Math.abs(column1-column2)){
 			for(int row = Math.min(row1, row2); row <= Math.max(row1, row2); row++){
 				if(get(row,column) != GRASS){
@@ -60,24 +74,20 @@ public class Field {
 				}
 				field[row][column] = fill;
 			}
-			fill++;
 			if(column1-column2 == 0){
-				break;
+				column++;
 			}
 		}
-		fill = 2;
 		for(int row = Math.min(row1, row2); row <= Math.max(row1, row2); row += Math.abs(row1-row2)){
 			for(int column = Math.min(column1, column2); column <= Math.max(column1, column2); column++){
 				if(get(row,column) != GRASS){
 					continue;
 				}
 				field[row][column] = fill;
-
 			}
-			fill++;
 			if(row1-row2 == 0){
-				break;
-			}			
+				row++;
+			}
 		}
 	}
 	public void addBoundaries(){
@@ -93,7 +103,27 @@ public class Field {
 				}
 			}
 		}
-		
+	}
+	public void searcher(int row, int column, int boundaries){
+		for(int r = -1; r <= 1; r++){
+			for(int c = -1; c<= 1; c++){
+				if(row+r < 0 || row+r >= field.length || column+c <0 || column+c >= field[0].length)
+					continue;
+				if(field[row+r][column+c] == GRASS){
+					field[row+r][column+c] = ACCESS;
+					searcher(row+r,column+c,boundaries);
+				}
+//				if(field[row+r][column+c] == STOP){
+//					if(boundaries>0){
+//						continue;
+//					}
+//					boundaries++;
+//					field[row+r][column+c] = ACCESS;
+//					searcher(row+r,column+c,boundaries);
+//
+//				}
+			}
+		}
 	}
 }
 
